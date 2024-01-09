762b4ec7e3caf686de33b7403a30953420ab8d08
ysolarh <ysolarh@gmail.com>
2024-01-10 00:12:33 +0900

    Feat: add function export_to_csv
    data 형식 만들어서 csv로 내보내는 함수

---
+++ day2_lotto/check_lotto/export.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def export_to_csv(st, winning_nums):
+    columns = ['회차', '추첨일', '1등', '1', '2', '3', '4', '5', '6', '보너스']
+    df = pd.DataFrame(columns=columns)
+    df.loc[0] = winning_nums
+    print(df)
+    df.to_csv(st + "회차.csv", index = False)
