ae1ee83e3d3d0fdded63fce0e8c6831cde530846
ysolarh <ysolarh@gmail.com>
2024-01-10 11:21:23 +0900

    Fix: fix the error in function compare_nums
    리스트 필요한 부분만 슬라이싱하고 테스트용 print문 추가

--- day2_lotto/check_lotto/compare.py
+++ day2_lotto/check_lotto/compare.py
@@ -2,10 +2,7 @@ import pandas as pd
 
 
 def get_user_input():
-    # user_input = input().split()
-    user_input = list(map(int, input().split()))
-    # print(user_input, type(user_input))
-    return user_input
+    return list(map(int, input().split()))
 
 
 def find_ranking(count, is_bonus):
@@ -37,37 +34,27 @@ def compare_nums(st):
     matched_nums = []
     user_input = get_user_input()
     df = pd.read_csv(st + '회차.csv')
-    # print(df.iloc[:, 3:].items())
-    # print(df.loc[:, '1':'보너스'])
-    # for col, num in df.iloc[:, 4:].items():
-    #     num = num.astype(int)
-    #     print("!", col, num, type(num))
-    #     if num in user_input:
-    #         if col == 6:
-    #             is_bonus = True
-    #         count += 1
-    #         matched_nums.append(num)
-
-    # for num in user_input:
-    #     print(df.iloc[:, 3:].isin([str(num)]))
-    #     if df.iloc[:, 3:].isin([num]):
-    #         # if col == 6:
-    #         #     is_bonus = True
-    #         count += 1
-    #         matched_nums.append(num)
-
-    # print(user_input)
-    # print(type(df.iloc[0, 3]))
-    # print(df.iloc[:, 3:].isin(user_input))
-    # for i in df.iloc[:, 3:].isin(user_input).items():
-
-    include = list(df.iloc[:, 3:].isin(user_input).itertuples())
-    # print("include", include)
-    for i in range(len(include[0])):
-        if include[0][i]:
+    df_cut = df.iloc[:, 3:]
+    include = list(df_cut.isin(user_input).itertuples())[0][1:] #
+    for i in range(len(include)):
+        print("idx: ", i)
+        print("val:", df_cut.iloc[0, i], include[i])
+        if include[i]:
+            print("inc")
             if i == 6:
+                print("!")
                 is_bonus = True
             count += 1
-            matched_nums.append(df.iloc[0, i+2])
+            matched_nums.append(df_cut.iloc[0, i])
+    # include = list(df.iloc[:, 3:].isin(user_input).itertuples()) #
+    # for i in range(len(include[0])):
+    #     print(i)
+    #     if include[0][i]:
+    #         print(df.iloc[:, 3:].iloc[0, i])
+    #         if i == 7:
+    #             print("!")
+    #             is_bonus = True
+    #         count += 1
+    #         matched_nums.append(df.iloc[0, i + 2])
     ranking = find_ranking(count, is_bonus)
     display_result(ranking, matched_nums)
